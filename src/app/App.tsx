991eb31cd7cc7696a8fb99750612a0764d0cdf8d
Shreyash Fulzele <shreyashfulzele@la-siriuss-mac.local>
2023-07-26 11:50:59 +0530

    lazy-loading

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { useState, Suspense } from 'react'
+import React, { useState, Suspense } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../font.css'
@@ -10,6 +10,7 @@ import Screen from './components/Screen/Screen.component';
 import AppScreen from './components/AppScreen/AppScreen.component';
 
 function App() {
+  const AppScreens = React.lazy(()=>import('./components/AppScreen/AppScreen.component'))
   const counter = useSelector((state:RootState)=>{
     return state.appReducer.showNavScreen
   })
@@ -17,7 +18,7 @@ function App() {
   return (
     <>
      <Suspense fallback={<div>Loading</div>}>
-      <AppScreen/>
+      <AppScreens/>
      </Suspense>
     </>
   )
