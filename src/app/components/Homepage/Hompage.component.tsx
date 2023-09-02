1596cb21b7a1890fe4dd7bb8bc57b2d3bb30491f
Shreyash Fulzele <shreyashfulzele@la-siriuss-mac.local>
2023-09-02 16:51:28 +0530

    home-page-res

--- src/app/components/Homepage/Hompage.component.tsx
+++ src/app/components/Homepage/Hompage.component.tsx
@@ -26,7 +26,7 @@ const Homepage: React.FC<any> = () => {
   let count:number = 0;
   const audioRef:any = useRef(null);
   useLayoutEffect(() => {
-    // imageRef.current.style.filter = colorPallete[colorPalleteIndex].filter;
+    imageRef.current.style.filter = colorPallete[colorPalleteIndex].filter;
     // accentColor = colorPallete[colorPalleteIndex].colorHex;
   }, [colorPalleteIndex]);
   
@@ -45,59 +45,59 @@ const Homepage: React.FC<any> = () => {
   })
 
   return (
-    <div></div>
-    // <section className="homepage">
-    //   <h1 className="homepage-head">Bonsoir!</h1>
-    //   <p>
-    //    "As a skilled MERN stack developer, I embark on a journey to bring ideas to life and transform digital landscapes. With a passion for crafting seamless experiences, I specialize in leveraging the power of MongoDB, Express.js, React, and Node.js to build dynamic, scalable, and intuitive web solutions. Armed with expertise in full-stack development, I thrive on pushing boundaries and exceeding client expectations." 
-    //   </p>
+    // <div></div>
+    <section className="homepage">
+      <h1 className="homepage-head">Bonsoir!</h1>
+      <p>
+       "As a skilled MERN stack developer, I embark on a journey to bring ideas to life and transform digital landscapes. With a passion for crafting seamless experiences, I specialize in leveraging the power of MongoDB, Express.js, React, and Node.js to build dynamic, scalable, and intuitive web solutions. Armed with expertise in full-stack development, I thrive on pushing boundaries and exceeding client expectations." 
+      </p>
      
-    //  <section className="bottom-container">
-    //   <div className="homepage-social-logos">
-    //     <a href="https://www.github.com/dopeUX" target="_blank">
-    //       <img
-    //         ref={imageRef}
-    //         className={`${colorPallete[colorPalleteIndex].name}`}
-    //         src="/assets/github-logo.svg"
-    //         alt=""
-    //       />
-    //     </a>
-    //     <a href="https://www.linkedin.com/in/shreyash-fulzele" target="_blank">
-    //       <img
-    //         ref={imageRef}
-    //         className={`${colorPallete[colorPalleteIndex].name}`}
-    //         src="/assets/linkedin-logo.svg"
-    //         alt=""
-    //       />
-    //     </a>
-    //   </div>
-    //   {/* <audio id="backgroundMusic" ref={audioRef} autoPlay={true} src="/public/assets/samplemusic.mp3" controls muted={false}></audio> */}
-    //   {/* <a href="" onClick={(e)=>{
-    //      e.preventDefault();
-    //   }}>
-    //   </a> */}
-    //   <section className="bottom-right-hlinks">
-    //     <a href="">
-    //       <p>Marketplace.</p>
-    //     </a>
-    //     <a href="">
-    //       <p>Read.</p>
-    //     </a> 
-    //     </section>  
-    //     {/* <OutlinedButton
-    //       title="Resume"
-    //       classN="homepage-download-btn"
-    //       color={accentColor}
-    //     >
-    //       <img
-    //         className={`download-btn-icon ${colorPallete[colorPalleteIndex].name}`}
-    //         src="/public/assets/download.svg"
-    //         alt=""
-    //       />
-    //     </OutlinedButton> */}
-    //     {/* <button className="outlined-btn homepage-download-btn">Resume <img className="download-btn-icon" src="/public/assets/download.svg" alt="" /></button> */}
-    //     </section>
-    // </section>
+     <section className="bottom-container">
+      <div className="homepage-social-logos">
+        <a href="https://www.github.com/dopeUX" target="_blank">
+          <img
+            ref={imageRef}
+            className={`${colorPallete[colorPalleteIndex].name}`}
+            src="/assets/github-logo.svg"
+            alt=""
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/shreyash-fulzele" target="_blank">
+          <img
+            ref={imageRef}
+            className={`${colorPallete[colorPalleteIndex].name}`}
+            src="/assets/linkedin-logo.svg"
+            alt=""
+          />
+        </a>
+      </div>
+      {/* <audio id="backgroundMusic" ref={audioRef} autoPlay={true} src="/public/assets/samplemusic.mp3" controls muted={false}></audio> */}
+      {/* <a href="" onClick={(e)=>{
+         e.preventDefault();
+      }}>
+      </a> */}
+      <section className="bottom-right-hlinks">
+        <a href="">
+          <p>Marketplace.</p>
+        </a>
+        <a href="">
+          <p>Read.</p>
+        </a> 
+        </section>  
+        {/* <OutlinedButton
+          title="Resume"
+          classN="homepage-download-btn"
+          color={accentColor}
+        >
+          <img
+            className={`download-btn-icon ${colorPallete[colorPalleteIndex].name}`}
+            src="/public/assets/download.svg"
+            alt=""
+          />
+        </OutlinedButton> */}
+        {/* <button className="outlined-btn homepage-download-btn">Resume <img className="download-btn-icon" src="/public/assets/download.svg" alt="" /></button> */}
+        </section>
+    </section>
   );
 };
 
